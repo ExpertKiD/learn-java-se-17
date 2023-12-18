1d77042ce82327835c5462c22d17e7f26c47d167
Suman Adhikari <expertxkid@gmail.com>
2023-12-18 20:15:13 +0545

    feat: add variable initialization

---
+++ _00002_two_variables_initializations/src/main/java/VariableInitializationApp.java
@@ -0,0 +1,9 @@
+public class VariableInitializationApp {
+    public static void main(String[] args) {
+        int myFirstNumber = (10 * 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    }
+}
