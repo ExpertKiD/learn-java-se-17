2371d661462f0e71438552819fe3317fb28f09f3
Suman Adhikari <expertxkid@gmail.com>
2023-12-18 21:21:10 +0545

    feat: add data types width

---
+++ _00004_byte_short_int_and_longs/src/main/java/WholeNumbersDataTypes.java
@@ -0,0 +1,24 @@
+public class WholeNumbersDataTypes {
+
+    public static void main(String[] args) {
+        System.out.println("Byte Data Type:: " +
+                "Min Value = " + Byte.MIN_VALUE + " "
+                + "Max Value = " + Byte.MAX_VALUE + " "
+                + " Width = " + Byte.SIZE);
+
+        System.out.println("Short Data Type:: " +
+                "Min Value = " + Short.MIN_VALUE + " "
+                + "Max Value = " + Short.MAX_VALUE + " "
+                + " Width = " + Short.SIZE);
+
+        System.out.println("Integer Data Type:: " +
+                "Min Value = " + Integer.MIN_VALUE + " "
+                + "Max Value = " + Integer.MAX_VALUE + " "
+                + " Width = " + Integer.SIZE);
+
+        System.out.println("Long Data Type:: " +
+                "Min Value = " + Long.MIN_VALUE + " "
+                + "Max Value = " + Long.MAX_VALUE + " "
+                + " Width = " + Long.SIZE);
+    }
+}
