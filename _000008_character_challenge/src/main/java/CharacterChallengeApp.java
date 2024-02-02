4dd2fbed82735c898ca82e0dc55fba314505e7d5
Suman Adhikari <expertxkid@gmail.com>
2024-02-02 11:31:40 -0600

    feat: add CharacterChallengeApp

---
+++ _000008_character_challenge/src/main/java/CharacterChallengeApp.java
@@ -0,0 +1,10 @@
+public class CharacterChallengeApp {
+    public static void main(String[] args) {
+        // Unicode site: https://unicode-table.com
+        char mySimpleChar = '?';
+        char myUnicodeChar = '\u003F';
+        char myDecimalChar = 63;
+
+        System.out.println(mySimpleChar +" "+ myUnicodeChar + ' '+ myDecimalChar);
+    }
+}
