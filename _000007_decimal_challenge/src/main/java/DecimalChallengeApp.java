0b1506367910b970db163f0671fb6765f511b9a4
Suman Adhikari <expertxkid@gmail.com>
2024-02-02 11:31:15 -0600

    feat: add DecimalChallengeApp

---
+++ _000007_decimal_challenge/src/main/java/DecimalChallengeApp.java
@@ -0,0 +1,10 @@
+public class DecimalChallengeApp {
+
+    public static void main(String[] args) {
+        double pounds = 200;
+
+        double kgs = pounds * 0.45359237f;
+
+        System.out.println("Pounds: "+pounds+" lbs  Kgs: "+kgs+" kgs");
+    }
+}
